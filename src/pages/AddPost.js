9f93b1dd495cb860fc73448d34ad94a33068b20c
Jiyoung Jenna Yeo <ysy91231@gmail.com>
2021-04-16 00:40:54 +0900

    입력 기능 구현 중

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import { Input } from "@material-ui/core";
 
 const AddPost = (props) => {
     // const post_list = useSelector(state => state.post.list);
+    const {history} = props;
+
+    // 포스트 추가 기능
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [comment, setComment] = useState("");
+    // const [contents, setContents] = React.useState("");
+    // 포스트 추가 함수 만들기
+    // const changeContents = (e) => {
+    //     setContents(e.target.value);
+    // }
+    // console.log(contents);
+    const newTitle = (e) => {
+        setTitle(e.target.value);
+    }
+    const newAuthor = (e) => {
+        setAuthor(e.target.value);
+    }
+    const newComment = (e) => {
+        setComment(e.target.value);
+    }
+    console.log(title,author,comment);
+    // const setPost = () => {
+    //     if(!word || !description || !example){
+    //       window.alert("모두 입력해주세요");
+    //       return;
+    //     }
     return (
         <React.Fragment>
                 <InputBox
-                    label="글제목"
+                    // 함수 먼저 만들고 _onChange로 글 넘겨줌
+                    // _onChange = {(e) =>{
+                    //     console.log("제목추가");
+                    //     newTitle(e.target.value);}}
+                    onChange={newTitle}
+                    value={title}
+                    label="title"
                     placeholder="제목을 입력하세요"
                     // value={}
                 />
                 <InputBox
-                    label="글쓴이"
-                    placeholder="글쓴이를 입력하세요"
+                    onChange = {newAuthor}
+                    value={author}
+                    label="author"
+                    placeholder="작성자를 입력하세요"
                     // value={}
                 />
             <Contents
-                label="글내용"
+                onChange = {newComment}
+                value={comment}
+                label="comment"
                 placeholder="내용을 입력하세요"
                 // value={}
             />
